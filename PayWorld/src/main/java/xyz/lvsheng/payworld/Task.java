9e56f5f92ad6c096a952c08edb6ab7fea1870e24
MrLv0816 <1525249038@qq.com>
2021-09-07 14:38:37 +0800

    优化计时器,改为取世界内玩家遍历

--- PayWorld/src/main/java/xyz/lvsheng/payworld/Task.java
+++ PayWorld/src/main/java/xyz/lvsheng/payworld/Task.java
@@ -3,12 +3,10 @@ package xyz.lvsheng.payworld;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
-import xyz.lvsheng.payworld.event.Listener;
 import xyz.lvsheng.payworld.utils.SQLite;
 import xyz.lvsheng.payworld.utils.Utils;
 
 import java.sql.SQLException;
-import java.util.Objects;
 
 
 /**
@@ -20,32 +18,21 @@ public class Task extends BukkitRunnable {
 
     @Override
     public void run() {
-        for (Player players : Bukkit.getOnlinePlayers()) {
+        for (String world : PayWorld.plugins.getConfig().getStringList("World")) {
+            for (Player player : Bukkit.getWorld(world).getPlayers()) {
 
-            //刚进入某个付费世界的玩家跳过一次扫描,以防被瞬间传送出去
-            if (!Objects.equals(Listener.playerMap.get(players.getUniqueId()), null) && Listener.playerMap.get(players.getUniqueId())) {
-                Listener.playerMap.put(players.getUniqueId(), false);
-                continue;
-            }
-
-
-            for (String s : PayWorld.plugins.getConfig().getStringList("World")) {
-
-
-                if (players.getWorld().getName().equalsIgnoreCase(s)) {
-                    try {
-                        int time = SQLite.select(PayWorld.sql, players.getUniqueId(), players.getWorld().getName());
-                        SQLite.update(PayWorld.sql, players.getUniqueId(), players.getWorld().getName(), --time);
-                        if (time < 1) {
-                            Bukkit.getScheduler().runTask(PayWorld.plugins, new Thread(() -> players.teleport(Utils.searchWorld(PayWorld.plugins.getConfig().getString("BackWorld")).getSpawnLocation())));
-                            players.sendMessage(Utils.ColorMessage(PayWorld.plugins.getConfig().getString("Messages.timeOut")));
-                        }
-                    } catch (SQLException throwables) {
-                        throwables.printStackTrace();
+                try {
+                    int pTime = SQLite.select(PayWorld.sql, player.getUniqueId(), world);
+                    SQLite.update(PayWorld.sql, player.getUniqueId(), world, --pTime);
+                    if (pTime < 1){
+                        player.sendMessage
+                                (Utils.ColorMessage(PayWorld.plugins.getConfig().getString("Messages.timeOut")));
+                        player.teleport(Utils.searchWorld(PayWorld.plugins.getConfig().getString("BackWorld")).getSpawnLocation());
                     }
-
-
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
                 }
+
             }
         }
     }
