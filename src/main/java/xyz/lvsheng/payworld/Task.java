ef983c87add86e8a1069f20d308fcfe9e0811923
MrLv <lvsheng1314@foxmail.com>
2021-12-20 14:18:51 +0800

    删除无用注释

--- src/main/java/xyz/lvsheng/payworld/Task.java
+++ src/main/java/xyz/lvsheng/payworld/Task.java
@@ -57,27 +57,4 @@ public class Task extends BukkitRunnable {
 
         }
     }
-    //    for (String world : PayWorld.plugins.getConfig().getStringList("World")) {
-    //        try{
-    //            for (Player player : Objects.requireNonNull(Bukkit.getWorld(world)).getPlayers()) {
-    //                //检测玩家时间
-    //                try {
-    //
-    //                    int pTime = SQLite.select(PayWorld.sql, player.getUniqueId(), world);
-    //                    SQLite.update(PayWorld.sql, player.getUniqueId(), world, --pTime);
-    //                    if (pTime < 1) {
-    //                        player.sendMessage
-    //                                (Utils.colorMessage(PayWorld.plugins.getConfig().getString("Messages.timeOut")));
-    //                        Bukkit.getScheduler().runTask(PayWorld.plugins, new Thread(() -> player.teleport(Utils.searchWorld(PayWorld.plugins.getConfig().getString("BackWorld")).getSpawnLocation())));
-    //                    }
-    //
-    //
-    //                } catch (SQLException throwables) {
-    //                    throwables.printStackTrace();
-    //                }
-    //            }
-    //        }catch (NullPointerException e){
-    //            Bukkit.getConsoleSender().sendMessage(Utils.colorMessage(PayWorld.plugins.getConfig().getString("Messages.NoWorld")));
-    //        }
-    //    }
 }
